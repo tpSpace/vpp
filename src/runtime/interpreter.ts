18703696bfc30777b8cde165a2dd8b7ebcb72409
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-03 00:27:13 +0700

    its libber dish; interpreter adjust data type

--- src/runtime/interpreter.ts
+++ src/runtime/interpreter.ts
@@ -1,2 +1,14 @@
-import { ValueType, RuntimeVal } from './values.ts'
-import { NodeType, Stmt } from '../frontend/ast.ts'
+import { NumberVal, ValueType, RuntimeVal, NullVal } from './values.ts'
+import { NodeType, NumericLiteral, Stmt } from '../frontend/ast.ts'
+
+export function evaluate(astNode: Stmt): RuntimeVal {
+    switch (astNode.kind) {
+        case "NumericLiteral":
+            return {
+                value: ((astNode as NumericLiteral).value as number).toString(),
+                type: "number",
+            } as NumberVal;
+        default:
+            return { value: "null", type: "null" } as NullVal;
+    }
+}
